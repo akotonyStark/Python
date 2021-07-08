258926e01e48f73db3f4e39bceb28d9613b7a4ad
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-08 03:45:21 +0000

    Create 3-conditionalStatement.py

---
+++ 3-conditionalStatement.py
@@ -0,0 +1,21 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print('The weather is hot')
+    print('Drink plenty of water')
+elif is_cold:
+    print("It's a cold day")
+    print('Wear warm clothes')
+else:
+    print("It's a lovely day")
+print('Enjoy your day')
+
+price_of_house = 1000000
+has_good_credits = True
+has_high_income = True
+
+if has_high_income and has_good_credits:
+    print('Down Payment = $', 0.1 * price_of_house)
+else:
+    print(f'Down Payment =  ${0.2 * price_of_house}')
