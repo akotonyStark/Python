30a17096f72df23b0d8870b08b02564211a1d814
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-08 03:21:20 +0000

    Create 0-intro.py

---
+++ 0-intro.py
@@ -0,0 +1,21 @@
+print('Augustine')
+print('*' * 10)
+
+price = 10
+final_price = 10 * price
+
+print(final_price)
+
+patient_name = 'John Smith'
+age = 20
+isNew = True
+
+name = input('What is your name? ')
+print('Hi ' + name)
+# if name == patient_name:
+#     print('Hi ' + name)
+
+
+weight = input('What is your weight in lbs? ')
+weight_in_kg = float(weight) / 2.2
+print('Your weight in kgs is ' + str(weight_in_kg))
