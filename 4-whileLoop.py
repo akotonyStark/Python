b82551f645ac97459d57f8c311ab51abe7b01e82
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-11 14:07:19 +0000

    Update 4-whileLoop.py

--- 4-whileLoop.py
+++ 4-whileLoop.py
@@ -3,3 +3,12 @@ while i <= 5:
     print('*' * i, i)
     i = i + 1
 print('Done')
+
+secret_number = 7
+count = 0
+guess_limit = 3
+while count < guess_limit:
+    number = int(input('Enter a number:'))
+    count += 1
+    if number == secret_number:
+        print('You won!!!')
