53da827e8f2abf41d3284c020574a02d01f0ecdd
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-07-11 16:33:01 +0000

    Update 6-forLoop.py

--- 6-forLoop.py
+++ 6-forLoop.py
@@ -1,5 +1,14 @@
 for letter in 'Augustine':
     print(letter)
 
-for item in [2,4,5,6,7,8]:
+for item in [2, 4, 5, 6, 7, 8]:
+    print(item)
+
+for item in range(10):
+    print(item)
+
+for item in range(5, 10):
+    print(item)
+
+for item in range(5, 10, 2):
     print(item)
